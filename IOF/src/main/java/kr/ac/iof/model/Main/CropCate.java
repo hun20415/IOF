f70b13e2e4073d6d6edddec634b94ecab5fffd71
Grace Lee <Grace Lee@Grace>
2015-05-20 16:19:12 +0900

    CropSpeciesInfo.java, complete, soglock

--- IOF/src/main/java/kr/ac/iof/model/Main/CropCate.java
+++ IOF/src/main/java/kr/ac/iof/model/Main/CropCate.java
@@ -22,37 +22,45 @@ public class CropCate {
 	@Id
 	@Column(name = "CROP_CATE_ID")//매핑되는 column이름
 	private int cropCateId;
+
 	@Column(name = "CROP_CATE_NAME", length = 200)
 	private String cropCateName;
+	
 	@Column(name = "CROP_CATE_HTML", length = 200)
 	private String cropCateHtml;
-	 public CropCate() {//생성자는 필수
+	 
+	public CropCate() {//생성자는 필수
 
-	    }
+	}
+	
 	public CropCate(String cropCateName, String cropCateHtml) {
 		super();
 		this.cropCateName = cropCateName;
 		this.cropCateHtml = cropCateHtml;
 	}
+	
 	public int getCropCateId() {
 		return cropCateId;
 	}
+	
 	public void setCropCateId(int cropCateId) {
 		this.cropCateId = cropCateId;
 	}
+	
 	public String getCropCateName() {
 		return cropCateName;
 	}
+	
 	public void setCropCateName(String cropCateName) {
 		this.cropCateName = cropCateName;
 	}
+	
 	public String getCropCateHtml() {
 		return cropCateHtml;
 	}
+	
 	public void setCropCateHtml(String cropCateHtml) {
 		this.cropCateHtml = cropCateHtml;
 	}
-
-	
 	
 }
