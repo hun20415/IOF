0b1aacac25a8cf0d6a2a5e31ae1d75487269730a
hun20415 <hun20415@gmail.com>
2015-06-02 19:43:18 +0900

    문제점 해결

--- IOF/src/main/java/kr/ac/iof/controller/ChartController.java
+++ IOF/src/main/java/kr/ac/iof/controller/ChartController.java
@@ -1,40 +1,21 @@
 package kr.ac.iof.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import kr.ac.iof.graph.demo.LineChart;
-import kr.ac.iof.main.Service.CropCateService;
-import kr.ac.iof.main.Service.FarmInfoService;
-import kr.ac.iof.model.User;
-import kr.ac.iof.model.UserGroup;
-import kr.ac.iof.model.Main.FarmEquipType;
-import kr.ac.iof.model.Main.FarmSectionInfo;
-import kr.ac.iof.service.UserService;
-import kr.ac.iof.service.UserGroupService;
-import kr.ac.iof.util.HibernateUtil;
 
-import org.hibernate.Session;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.servlet.ServletUtilities;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.general.DatasetUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
@@ -67,7 +48,7 @@ public class ChartController {
 			chart = LineChart
 					.createLineChart("Line Chart", "x축", "y축", dataset);
 		}
-		// ServletUtilities是面向web开发的工具类，返回一个字符串文件名,文件名自动生成，生成好的图片会自动放在服务器（tomcat）的临时文件下（temp）
+
 		String filename = null;
 		try {
 			filename = ServletUtilities.saveChartAsPNG(chart, 800, 400,
@@ -76,7 +57,7 @@ public class ChartController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// 根据文件名去临时目录下寻找该图片，这里的/DisplayChart路径要与配置文件里用户自定义的<url-pattern>一致
+
 		String graphURL = request.getContextPath() + "/DisplayChart?filename="
 				+ filename;
 		request.setAttribute("imgurl", graphURL);
